6e3ce5ffa66ccbbfbade4d6ca9cfcfb6732ca12b
hari1016 <63118506+hari1016@users.noreply.github.com>
2020-04-30 00:02:42 +0530

    Create Fibonacci.java

---
+++ Fibonacci.java
@@ -0,0 +1,17 @@
+package fibonacci;
+import java.lang.*;
+import java.util.*;
+public class Fibonacci {
+    public static void main(String[] args) {
+        int a=0,b=1,c;
+        System.out.print(a+" "+b+" ");
+        for(int i=0;i<20;i++)
+        {
+            c=a+b;
+            System.out.print(c+" ");
+            a=b;
+            b=c;
+        }
+    } 
+    
+}
